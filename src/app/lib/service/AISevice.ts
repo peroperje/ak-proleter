a86038b188dcd5f92d69fcddfff7ebf47d8e3470
Petar B <el.pionir@gmail.com>
2025-09-03 15:58:38 +0200

    refactored AIServices

--- src/app/lib/service/AISevice.ts
+++ src/app/lib/service/AISevice.ts
@@ -1,8 +1,6 @@
-
-import { InferenceClient } from "@huggingface/inference";
+import { InferenceClient } from '@huggingface/inference';
 import { AthleteFormData } from '@/app/lib/actions';
 import { useMemo } from 'react';
-import { string } from 'yup';
 
 
 export class AIService {
@@ -17,26 +15,27 @@ export class AIService {
 
   // Text processing using Hugging Face with a working model
   async extractAthleteData(prompt: string): Promise<AthleteFormData | undefined> {
-    // Try multiple models in order of preference
-    const models = [
-      "deepseek-ai/DeepSeek-V3-0324",
-    ];
-
-
-    for (const model of models) {
       try {
-        const result = await this.tryModel(model, prompt);
-        if (result) return result;
+        const result = await this.tryDeepSeekModel( prompt);
+        if (result) {
+          const json = await JSON.parse(result);
+          const dateOfBirth = new Date(json.dateOfBirth);
+          if (json) {
+            return {
+              ...json,
+              dateOfBirth: dateOfBirth.toString() !== 'Invalid Date' ? dateOfBirth : undefined,
+            };
+          }
+        }
       } catch (error) {
-        console.error(`Error with model ${model}:`, error);
-        console.warn(`Model ${model} failed, trying next...`);
+        console.error(`Error with model DeepSeekModel:`, error);
       }
-    }
 
     // If all models fail, use fallback extraction
   }
 
-  private async tryModel(model: string, prompt: string): Promise<AthleteFormData | null> {
+  private async tryDeepSeekModel( prompt: string): Promise<string | undefined> {
+    const model ="deepseek-ai/DeepSeek-V3-0324";
     const systemPrompt = `${this.defaultPrompt}
 
 Text: "${prompt}"
@@ -47,12 +46,12 @@ JSON:`;
       model: model,
       messages: [
         {
-          role: "user",
+          role: 'user',
           content: systemPrompt,
         },
       ],
       response_format: {
-        type: "json_object",
+        type: 'json_object',
       },
       temperature: 0.1,
       max_tokens: 100,
@@ -61,21 +60,7 @@ JSON:`;
       presence_penalty: 0.0,
       stream: false,
     });
-
-    const content = chatCompletion.choices[0].message.content;
-
-
-    if (content){
-        const json = await JSON.parse(content);
-      const dateOfBirth = new Date(json.dateOfBirth);
-      if(json){
-        return {
-          ...json,
-          dateOfBirth: dateOfBirth.toString() !== 'Invalid Date' ? dateOfBirth:undefined,
-        }
-      }
-    }
-    return null;
+    return chatCompletion.choices[0].message.content;
   }
 
 
