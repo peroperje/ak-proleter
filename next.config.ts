386ee59d79a934e0b6607fcef55d537ab02a0965
Petar B <el.pionir@gmail.com>
2025-12-29 16:53:02 +0100

    try to fix prizma problem on vercel

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   serverExternalPackages: ['@prisma/client'],
+  outputFileTracingIncludes: {
+    '/api/**/*': ['./node_modules/.prisma/client/**/*'],
+    '/*': ['./node_modules/.prisma/client/**/*'],
+  },
 };
 
 export default nextConfig;
